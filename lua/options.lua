ffcdf89cdfcb3998dcb27536a03531374d50024b
Mario Nietzsche <mario.n@myronaxia.org>
2024-05-22 17:22:22 +0200

    more

--- lua/options.lua
+++ lua/options.lua
@@ -1,22 +1,24 @@
-vim.opt.mouse = "a"
-vim.opt.clipboard = "unnamedplus"
+-- some basic options
 
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.opt.undofile = true
-vim.opt.showmode = false
-vim.opt.inccommand = "split"
+-- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.opt.wrap = true
-
+-- indenting
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.termguicolors = false
+
+-- window splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- misc
+vim.opt.hlsearch = true
+vim.opt.undofile = true
+vim.opt.inccommand = "split"
+vim.opt.wrap = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
--- lua/style.lua
+++ lua/style.lua
@@ -1,3 +1,5 @@
 -- basic style settings
 vim.g.have_nerd_font = true
 vim.g.have_devicons = true
+vim.opt.termguicolors = false
+vim.opt.showmode = true

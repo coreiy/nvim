6f45b5db0a4e60edda786f4de8962a66a8ecd561
Mario Nietzsche <mario.n@myronaxia.org>
2024-05-22 17:06:29 +0200

    style

--- init.lua
+++ init.lua
@@ -3,14 +3,11 @@
 -- leader key
 vim.g.mapleader = " "
 
+-- load some basic style settings from ./style.lua
+require("style")
 -- load options from ./options.lua
 require("options")
-
-vim.g.have_nerd_font = true
-vim.g.have_devicons = true
-
 -- load keymaps from ./keymaps.lua
 require("keymaps")
-
 -- load lazy.nvim setup from ./my_lazy_setup.lua
 require("my_lazy_setup")
---
+++ lua/style.lua
@@ -0,0 +1,3 @@
+-- basic style settings
+vim.g.have_nerd_font = true
+vim.g.have_devicons = true

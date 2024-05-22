5c5e7120ce81a13b17143c7a1e79b28c6078ed8d
Mario Nietzsche <mario.n@myronaxia.org>
2024-05-22 17:00:08 +0200

    fixed broken setup

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -5,18 +5,12 @@ return {
 		{
 			"L3MON4D3/LuaSnip",
 			build = (function()
-				-- Build Step is needed for regex support in snippets.
-				-- This step is not supported in many windows environments.
-				-- Remove the below condition to re-enable on windows.
 				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
 					return
 				end
 				return "make install_jsregexp"
 			end)(),
 			dependencies = {
-				-- `friendly-snippets` contains a variety of premade snippets.
-				--    See the README about individual language/framework/plugin snippets:
-				--    https://github.com/rafamadriz/friendly-snippets
 				{
 					"rafamadriz/friendly-snippets",
 					config = function()
@@ -26,15 +20,10 @@ return {
 			},
 		},
 		"saadparwaiz1/cmp_luasnip",
-
-		-- Adds other completion capabilities.
-		--  nvim-cmp does not ship with all sources by default. They are split
-		--  into multiple repos for maintenance purposes.
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
 	},
 	config = function()
-		-- See `:help cmp`
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 		luasnip.config.setup({})
